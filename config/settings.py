babf60bfa8de8311f4a81bf68e23a83d5e0ddf9d
JordiBanuelos99 <banuelosmendez99@gmail.com>
2022-08-21 11:34:48 -0700

    heroku update

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m_k)weub&v#_ctulvgrt^opa%pn_g!(_xidmdbtj8zbktss%$l'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
@@ -126,3 +130,17 @@ STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
+
+EMAIL_BACKEND = (
+    'django.core.mail.backends.%s.EmailBackend'
+    % env.str("EMAIL_BACKEND", default="smtp")
+)
+
+DEFAULT_FROM_EMAIL = env.str("DEFAULT_FROM_EMAIL")
+EMAIL_POST = env.str("EMAIL_HOST")
+EMAIL_HOST_USER = env.str("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,17 @@
 # Create your views here.
+from ast import Return
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import (
 	CreateView,
 	UpdateView,
 	DeleteView
 )
+
+from django.contrib.auth.mixins import (
+	LoginRequiredMixin, UserPassesTestMixin
+)
+
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from django.urls import reverse_lazy
 from .models import Post
@@ -17,18 +24,29 @@ class PostDetailView(DetailView):
 	template_name = "posts/detail.html"
 	model = Post
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
 	template_name = "posts/new.html"
 	model = Post
-	fields = ["title", "subtitle", "body", "author"]
+	fields = ["title", "subtitle", "body"]
 
-class PostUpdateView(UpdateView):
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 	template_name = "posts/edit.html"
 	model = Post
 	fields = ["title", "subtitle", "body"]
 
-class PostDeleteView(DeleteView):
+	def test_func(self):
+		obj = self.get_object()
+		return obj.author==self.request.user
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     template_name = "posts/delete.html"
     model = Post
     success_url = reverse_lazy("post_list")
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author==self.request.user
